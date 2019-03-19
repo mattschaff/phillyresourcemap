7862c0939849b74329ea322d84f8a9d8b31161f9
Matt Schaff <matthew.schaff@gmail.com>
2019-03-19 07:55:10 -0400

    theme improvements

---
+++ modules/custom/link_core/src/Controller/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\link_core\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides a Dashboard route callback for /dashboard
+ */
+class DashboardController extends ControllerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDashboard() {
+    return [];
+  }
+
+}
